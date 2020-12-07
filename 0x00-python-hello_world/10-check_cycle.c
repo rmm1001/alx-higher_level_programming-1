15ae1bcbde511214e633a2bd7e4c8d665fca707c
Emmastro <emurairi18@alastudents.org>
2020-12-07 14:00:13 +0100

    Fix errors

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -2,31 +2,20 @@
 #include <stdio.h>
 
 /**
- * check_cycle - checks if a singly linked list has a cycle in it.
- * Description: a singly list has a cycle when one of the elements within
- * the list points to the head address
+ * check_cycle - checks if a singly linked list has a cycle in it
  * @list: singly list to check
  * Return: 0 if there is no cycle, 1 if there is a cycle
  */
 int check_cycle(listint_t *list)
 {
-	/**
-	 * Step to explain on the interviewer:
-	 * save the head memory address on a temp variable
-	 * go over the singly list check if the node is pointing to the head
-	 * If yes, return 1, if no, continue.
-	 * Return 0 if the loop when until the end (did not find any
-	 * case where the node was pointing to the head)
-	*/
-	listint_t *head;
+
 	listint_t *current;
 
-	head = list;
-	current = head->next;
+	current = list->next;
 
 	while (current != NULL)
 	{
-		if (current == head)
+		if (current == list)
 			return (1);
 		current = current->next;
 	}
