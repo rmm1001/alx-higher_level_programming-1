d6620ab48d971f719b152970aaf1cc4647311add
Emmastro <emurairi18@alastudents.org>
2020-12-08 15:35:41 +0100

    sumbit advaced C: insert node on sorted list

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -5,31 +5,35 @@
  * insert_node -  inserts a number into a sorted singly linked list
  * @head: singly list head
  * @number: number to insert
- * Return: the address of the new node, or NULL if it failed    
+ * Return: the address of the new node, or NULL if it failed
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-    listint_t *new;
-    listint_t *current;
+	listint_t *new;
+	listint_t *current;
 
-    current = *head;
+	current = *head;
 
-    new = malloc(sizeof(listint_t));
-    if (new == NULL)
-        return (NULL);
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
 
-    new->n = number;
-    new->next = NULL;
+	new->n = number;
 
-    if (*head == NULL)
-        *head = new;
-    else
-    {
-        while (current->next != NULL)
-            current = current->next;
-        current->next = new;
-    }
+	if (*head == NULL)
+		*head = new;
+	else
+	{
+		while (current->next != NULL)
+		{
+			if (current->next->n > number)
+				break;
 
-    return (new);
+			current = current->next;
+		}
+		new->next = current->next;
+		current->next = new;
+	}
 
+	return (new);
 }
