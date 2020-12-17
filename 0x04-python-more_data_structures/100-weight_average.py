e58b505e95d121c6e569a13bb8769810d1d3ae06
Emmastro <emurairi18@alastudents.org>
2020-12-17 20:42:30 +0100

    Submit

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,4 +2,4 @@
 def weight_average(my_list=[]):
     if len(my_list) == 0:
         return 0
-    return sum([x*y for (x,y) in my_list]) / sum([y for (x,y) in my_list])
+    return sum([x*y for (x, y) in my_list]) / sum([y for (x, y) in my_list])
