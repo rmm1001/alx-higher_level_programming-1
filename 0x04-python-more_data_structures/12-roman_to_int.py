d8078dd5822ea23f2d8feab5b8b330a5b92ca0ad
Emmastro <emurairi18@alastudents.org>
2020-12-17 18:30:37 +0100

    Submit

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,7 +6,7 @@ def roman_to_int(roman_string:str):
     data = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
     numbers = [data[x] for x in roman_string]
     print(numbers)
-    #for x in range(len(data)):
+    for x in range(len(data)):
 
 roman_number = "LXXXVII"
 roman_to_int(roman_number)
