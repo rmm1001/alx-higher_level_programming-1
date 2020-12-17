2158b31ec2cdd6035e36665313b629e2e0f79592
Emmastro <emurairi18@alastudents.org>
2020-12-17 19:10:33 +0100

    Submit

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,5 +2,5 @@
 def square_matrix_simple(matrix=[]):
     squared = []
     for line in matrix:
-        squared.append([ c**2 for c in line])
+        squared.append([c**2 for c in line])
     return squared
--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,13 +1,30 @@
 #!/usr/bin/python3
-def roman_to_int(roman_string:str):
+def roman_to_int(roman_string: str):
     if not roman_string:
         return
-    rep = 0
-    data = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
-    numbers = [data[x] for x in roman_string]
-    print(numbers)
-    for x in range(len(data)):
+    data = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    numbers = [data[x] for x in reversed(roman_string)]
+    rep = numbers.pop(0)
+
+    for x in numbers:
+        #print(rep, x, x >= rep)
+        if x >= rep:
+            rep += x
+        else:
+            rep -= x
+    return rep
+
+roman_number = "X"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "MDCCLXXVI"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "IX"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
 
 roman_number = "LXXXVII"
-roman_to_int(roman_number)
-#print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "DCCVII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
