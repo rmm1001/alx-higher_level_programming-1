aa297b8a72891a53aa9b33d9dd06b3453c1ce8ce
Emmastro <emurairi18@alastudents.org>
2020-12-17 19:55:33 +0100

    Submit

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,28 +3,13 @@ def roman_to_int(roman_string: str):
     if not roman_string:
         return
     data = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    numbers = [data[x] for x in reversed(roman_string)]
-    rep = numbers.pop(0)
+    numbers = [data[x] for x in roman_string] + [0]
+    rep = 0
 
-    for x in numbers:
-        #print(rep, x, x >= rep)
-        if x >= rep:
-            rep += x
+    for i in range(len(numbers) - 1):
+        if numbers[i] >= numbers[i+1]:
+            rep += numbers[i]
         else:
-            rep -= x
+            rep -= numbers[i]
+
     return rep
-
-roman_number = "X"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "MDCCLXXVI"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "IX"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "LXXXVII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "DCCVII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def simple_delete(my_dict, key=""):
-    my_dict.pop(key)
+    my_dict.pop(key, None)
     return my_dict
