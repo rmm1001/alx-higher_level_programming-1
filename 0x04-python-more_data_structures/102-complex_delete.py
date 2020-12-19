bae56c9ed050564dc2ca36e6b4ddf90437797965
Emmastro <emurairi18@alastudents.org>
2020-12-19 12:18:35 +0100

    Submit

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def complex_delete(my_dict, value):
-    return {k: v for k, v in my_dict.items() if value not in v}
+    tmp = my_dict.copy()
+    for k, v in tmp.items():
+        if value in v:
+            my_dict.pop(value)
+    return my_dict
