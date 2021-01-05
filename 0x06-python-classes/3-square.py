df34a9fb742086668c7a47c63698f5ad3e123247
Emmastro <emurairi18@alastudents.org>
2021-01-05 09:06:12 +0100

    Implement square classes

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+class Square:
+    def __init__(self, size=0):
+        self.__size = size
+    
+    @property
+    def __size(self):
+        return self.__size
+
+    @__size.setter
+    def __size(self, size):
+        if type(size) != int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+    
+    def area(self):
+        return (self.__size ** 2)
