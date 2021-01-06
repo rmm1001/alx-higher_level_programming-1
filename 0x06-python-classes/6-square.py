7b69f6099e200c49be7fbb80a005f5c4b8c4dab1
Emmastro <emurairi18@alastudents.org>
2021-01-06 12:25:44 +0100

    Submit

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -38,11 +38,12 @@ class Square:
         """
         if (self.__size == 0):
             print('')
-        for i in range(self.position[1]):
-            print('')
+        else:
+            for i in range(self.position[1]):
+                print('')
 
-        for i in range(self.size):
-            print(' ' * self.position[0] + '#' * self.size)
+            for i in range(self.size):
+                print(' ' * self.position[0] + '#' * self.size)
 
     @property
     def position(self):
