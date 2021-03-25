273093a5765d0a5b7952c7663e21410d0b02fe1a
Emmanuel Murairi <emurairi18@alastudents.org>
2021-03-25 10:39:17 +0400

    Submit

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -6,10 +6,14 @@ const src1 = process.argv[2];
 const src2 = process.argv[3];
 const dest = process.argv[4];
 
-try {
-  const data1 = fs.readFile(src1);
-  const data2 = fs.readFile(src2);
-  fs.writeFile(dest, data1.toString() + data2.toString());
-} catch (error) {
-  console.error(`Got an error trying to read the file: ${error.message}`);
+function callback (err, data) {
+  if (err) {
+    return console.log(err);
+  }
+  fs.appendFile(dest, data, function (err) {
+    if (err) console.log(err);
+  });
 }
+
+fs.readFile(src1, 'utf8', callback);
+fs.readFile(src2, 'utf8', callback);
